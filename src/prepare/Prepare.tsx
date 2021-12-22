3acbb61c0c7a6fb6c8038354ae77f18298cd58f0
b1018043 <grey.fox.0417@gmail.com>
2021-12-22 23:04:40 +0900

    feat: use livekitroom

--- src/prepare/Prepare.tsx
+++ src/prepare/Prepare.tsx
@@ -2,18 +2,25 @@ import react,{ChangeEvent, MouseEventHandler, ReactEventHandler, useState} from 
 import { Button, TextField } from "@mui/material";
 import {useNavigate} from "react-router-dom";
 
+// リクエストの構造体 (正直いらない)
 interface TokenRequest{
     room: string;
     user: string;
 }
 
+// 通話前の画面
 const Prepare = () =>{
+    // 必要なstateを宣言
+    // ボタンを押しているかの管理ステート
     const [pressed,setPressed] = useState<boolean>(false);
+    // room名,user名の管理用ステート
     const [room,setRoom] = useState<string>("");
     const [username,setUserName] = useState<string>("");
 
+    // 画面遷移に必要なhookの呼び出し
     const navigate = useNavigate();
     
+    // handle~~系はstateの変更を行っている
     const handleRoom:ReactEventHandler<Element> = (e: ChangeEvent<HTMLInputElement>)=>{
         setRoom(e.target.value);
     }
@@ -23,21 +30,27 @@ const Prepare = () =>{
     }
 
     const clickJoinButton: MouseEventHandler = async () =>{
+        // buttonの無効化
         setPressed(p=>!p);
+        // リクエストの定義
         const req:TokenRequest = {
             room,
             user:username,
         } ;
+        // 通信処理の呼び出し
         await fetch("/token",{
             method: "POST",
             headers:{
                 'Content-Type': 'application/json',
             },
+            // bodyをつける際はPOSTにすること
             body: JSON.stringify(req)
         }).then(res=>res.json()).then((body)=>{
             if(body.errors){
                 throw body.errors;
             }
+            // ここで/room(Room.tsx)へ遷移する
+            // stateでtokenを渡す
             navigate("/room",{
                 state:{
                     token: body.token
@@ -53,6 +66,7 @@ const Prepare = () =>{
         <div>
             <TextField value={room} onChange={handleRoom}/>
             <TextField value={username} onChange={handleUserName}/>
+            {/* disabledではボタンのクリック処理中・room,userが未記入の際にtrueになる */}
             <Button 
                 disabled={pressed||room.length===0||username.length===0}
                 onClick={clickJoinButton}
--- src/room/Room.tsx
+++ src/room/Room.tsx
@@ -1,9 +1,29 @@
 import react from "react";
+import { useLocation } from "react-router-dom";
+import { LiveKitRoom} from "livekit-react";
+// Roomをコンポーネントとして定義しているので別の名前(RoomClass)に置き換えている
+import { Room as RoomClass} from "livekit-client";
+
+// Livekit-serverのurl(ws)
+const LiveKitServerURL: string = "ws://localhost:7880";
 
 const Room = ()=>{
+    // tokenを取得している
+    // NOTE: もっと綺麗なやり方があったら教えて欲しい
+    const location = useLocation();
+    const state: any= location.state;
+    const token: string = state.token;
+
+    const handleConnected = async (room: RoomClass) =>{
+        await room.localParticipant.setCameraEnabled(true);
+        await room.localParticipant.setMicrophoneEnabled(true);
+    }
+
     return (
         <div>
-            Room
+            <LiveKitRoom url={LiveKitServerURL} token={token}
+                onConnected={handleConnected}
+            /> 
         </div>
     )
 }
